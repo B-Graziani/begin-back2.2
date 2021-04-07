f7086afbc905bb4b43887d746903fcb3d1ff7bc8
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-07 11:36:15 +0200

    view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "destroy ts"
+Restaurant.destroy_all
+puts "creons ts"
+Restaurant1 = { name: "la tour d'argent", address: "24 rue du pont", star: "5"}
+Restaurant2 = { name: "mcdonald", address: "24 rue du pont", star: "0"}
+Restaurant3 = { name: "chez Wung", address: "24 rue du pont", star: "3"}
+Restaurant4 = { name: "ritz", address: "24 rue du pont", star: "4"}
+Restaurant5 = { name: "kfc", address: "24 rue du pont", star: "1"}
+Restaurants []
+[ Restaurant1, Restaurant2, Restaurant3, Restaurant4, Restaurant5].each_with_index do |attributes, index|
+ restaurant = Restaurant.create!(attributes)
+ Restaurants << restaurant
+end
